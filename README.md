b997ff79bb8002eb043f77c03baa9d612c02f52b
Khoa Phung <minhkhoablieu@gmail.com>
2024-04-18 00:15:20 +0700

    Update Script

---
+++ README.md
@@ -0,0 +1,17 @@
+# Crawl keyword redbubble.com
+
+```bash
+# Linux
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+$ cp .env.example .env
+$ python main.py
+
+# Windows
+$ python -m venv venv
+$ venv\Scripts\activate
+$ pip install -r requirements.txt
+$ cp .env.example .env
+$ python main.py
+```
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import os
 import cloudscraper
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
 
 scraper = cloudscraper.create_scraper()
 
 url_shop = 'https://www.redbubble.com'
 
+is_enable_mongo = os.getenv('ENABLE_MONGO', 'False') == 'True'
+client = MongoClient(os.getenv('ATLAS_URI'))
+db = client[os.getenv('DB_NAME')]
+collection = db[os.getenv('COLLECTION_NAME')]
 
-def download_url(url):
+# unique keyword and iaCode
+collection.create_index([('keyword', 1), ('iaCode', 1)], unique=True)
+
+
+def download_url(url: str) -> str:
     print(url)
     response = scraper.get(url)
     return response.text
@@ -22,17 +35,36 @@ class Crawler:
         self.urls_to_visit = urls
         self.keywords = set()
 
-    def get_keyword(self, content):
+    def get_keyword(self, content: str, url: str):
         soup = BeautifulSoup(content, 'html.parser')
 
-        links = [a['href'] for a in soup.select('div#TopicsCarousel a')]
+        # get all links from div.styles__resultsProductsContainer--1Xv9D > div.styles__carouselInner--3mwKu
+        links = [a['href'] for a in
+                 soup.select('div.styles__resultsProductsContainer--1Xv9D div.styles__carouselInner--3mwKu a')]
 
         # skip if link is already visited
         links = [link for link in links if link not in self.visited_urls]
         self.urls_to_visit.extend(links)
 
-        keywords = [span.text for span in soup.select('div#TopicsCarousel span')]
-        self.keywords.update(keywords)
+        # first remove the /shop/ from the links
+        keyword_and_category = [link.replace('/shop/', '') for link in links]
+
+        # remove keywords that are already in the set
+        keyword_and_category = [keyword for keyword in keyword_and_category if keyword not in self.keywords]
+        self.keywords.update(keyword_and_category)
+
+        # insert into mongo if enabled
+        if is_enable_mongo:
+            for keyword in keyword_and_category:
+                data = keyword.split('+')
+
+                iaCode = data.pop()
+                data = '+'.join(data).replace('+', ' ')
+
+                try:  # insert into mongo
+                    collection.insert_one({'keyword': data, 'iaCode': iaCode})
+                except Exception as e:
+                    print(e)
 
     def run(self):
         while self.urls_to_visit:
@@ -41,14 +73,15 @@ class Crawler:
             url_crawl = url_shop + url
             try:
                 content = download_url(url_crawl)
-                self.get_keyword(content)
-                print(self.keywords)
+                self.get_keyword(content, url)
                 self.visited_urls.add(url_crawl)
 
             except Exception as e:
                 print(e)
                 self.failed_urls.add(url_crawl)
 
+        print('Failed URLs:', self.failed_urls)
+
 
 if __name__ == '__main__':
-    Crawler(urls=['/shop/?iaCode=u-tees&query=Anniversary&ref=search_box']).run()
+    Crawler(urls=['/shop/funny+clothing']).run()
